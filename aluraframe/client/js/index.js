73ff7b3211116b42b979886ef520c91f31db0d61
Wilson Barbosa <wbarbosa.br@gmail.com>
2021-02-17 00:37:40 -0300

    Final da aula 2

--- aluraframe/client/js/index.js
+++ aluraframe/client/js/index.js
@@ -8,18 +8,18 @@ console.log(campos);
 
 var tbody = document.querySelector('table tbody');
 
-document.querySelector('.form').addEventListener('submit', function(event){
+document.querySelector('.form').addEventListener('submit', function(event) {
   event.preventDefault();
   var tr = document.createElement('tr');
 
-  campos.forEach(function(campo){
+  campos.forEach(function(campo) {
     var td = document.createElement('td');
     td.textContent = campo.value;
     tr.appendChild(td);
   });
 
   var tdVolume = document.createElement('td');
-  tdVolume.textContent = campos[1].value*campos[2].value;
+  tdVolume.textContent = campos[1].value * campos[2].value;
   tr.appendChild(tdVolume);
 
   tbody.appendChild(tr);
